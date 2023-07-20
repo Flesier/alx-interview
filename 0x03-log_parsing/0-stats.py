28a95fb3850218a627f11da600c9113d22fe38e9
Flesier <flesieronyango18@gmail.com>
2023-07-20 08:40:59 +0300

    metric computation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,48 +2,51 @@
 """
     script that reads stdin line by line and computes metrics
 """
+from collections import defaultdict
+import signal
 import sys
 
+def print_statistics(total_size, status_codes):
+    print(f"Total file size: File size: {total_size}")
+    for code in sorted(status_codes.keys()):
+        print(f"{code}: {status_codes[code]}")
 
-def print_msg(codes, file_size):
-    print("File size: {}".format(file_size))
-    for key, val in sorted(codes.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+def process_lines():
+    total_size = 0
+    status_codes = defaultdict(int)
+    line_count = 0
 
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            if not line:
+                continue
+            
+            # Parse the line and extract relevant information
+            parts = line.split()
+            if len(parts) < 7:
+                continue
+            
+            ip_address, _, _, status_code, file_size = parts[0], parts[3], parts[5], parts[6], parts[7]
+            
+            if not status_code.isdigit():
+                continue
 
-file_size = 0
-code = 0
-count_lines = 0
-codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+            # Update metrics
+            total_size += int(file_size)
+            status_codes[status_code] += 1
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
+            line_count += 1
 
-        if len(parsed_line) > 2:
-            count_lines += 1
+            # Print statistics every 10 lines
+            if line_count % 10 == 0:
+                print_statistics(total_size, status_codes)
 
-            if count_lines <= 10:
-                file_size += int(parsed_line[0])
-                code = parsed_line[1]
+    except KeyboardInterrupt:
+        pass
 
-                if (code in codes.keys()):
-                    codes[code] += 1
+    # Print final statistics
+    print_statistics(total_size, status_codes)
 
-            if (count_lines == 10):
-                print_msg(codes, file_size)
-                count_lines = 0
-
-finally:
-    print_msg(codes, file_size)
+if __name__ == "__main__":
+    process_lines()
